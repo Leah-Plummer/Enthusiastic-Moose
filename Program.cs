f89dc6da7e20dcd2697668bfae1c1c8e94744ae1
Leah Plummer <leahplumm@gmail.com>
2022-11-16 21:57:42 -0600

    refractored app to reduce the redundancy in the code

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿
+﻿using System.Linq;
 using System;
 
 Main();
@@ -13,11 +13,55 @@ void Main()
     MooseSays("H I, I'M  E N T H U S I A S T I C !");
     MooseSays("I really am enthusiastic");
 
-    // Ask a question
-    CanadaQuestion();
-    EnthusiasticQuestion();
-    LoveCSharpQuestion();
-    SecretQuestion();
+    List<string> questions = new List<string>() 
+{
+    "Is Canada real?",
+    "Are you enthusiastic?", 
+    "Do you love C# yet?", 
+    "Do you want to know a secret?",
+};
+
+List<string> yesses = new List<string>() 
+{
+    "Really? It seems very unlikely.",
+    "Yay!", 
+    "Good job sucking up to your instructor!", 
+    "ME TOO!!!! I love secrets...tell me one!",
+};
+
+List<string> nos = new List<string>() 
+{
+    "I  K N E W  I T !!!",
+    "You should try it!", 
+    "You will...oh, yes, you will...", 
+    "Oh, no...secrets are the best, I love to share them!",
+};
+
+int i = 0; 
+
+foreach (string question in questions)
+{
+    Console.WriteLine($"{question}(Y/N): ");
+    string answer = Console.ReadLine().ToLower();
+
+    while (answer != "y" && answer != "n")
+    {
+        Console.Write($"{question} (Y/N): ");
+        answer = Console.ReadLine().ToLower();
+    }
+
+     if (answer == "y") 
+    {
+        Console.WriteLine($"{yesses[i]}");
+        i++;
+    }
+    else
+    {
+        Console.WriteLine($"{nos[i]}") ;
+        i++;
+    }
+
+}
 }
 
 
@@ -53,78 +97,8 @@ void MooseSays(string message)
     ");
 };
 
-bool MooseAsks(string question)
-{
-    Console.Write($"{question} (Y/N): ");
-    string answer = Console.ReadLine().ToLower();
 
-    while (answer != "y" && answer != "n")
-    {
-        Console.Write($"{question} (Y/N): ");
-        answer = Console.ReadLine().ToLower();
-    }
 
-    if (answer == "y")
-    {
-        return true;
-    }
-    else
-    {
-        return false;
-    }
-}
-
-void CanadaQuestion()
-{
-    bool isTrue = MooseAsks("Is Canada real?");
-    if (isTrue)
-    {
-        MooseSays("Really? It seems very unlikely.");
-    }
-    else
-    {
-        MooseSays("I  K N E W  I T !!!");
-    }
-}
-
-void EnthusiasticQuestion()
-{
-    bool isEnthusiastic = MooseAsks("Are you enthusiastic?");
-    if (isEnthusiastic)
-    {
-        MooseSays("Yay!");
-    }
-    else
-    {
-        MooseSays("You should try it!");
-    }
-}
-
-void LoveCSharpQuestion()
-{
-    bool doesLoveCSharp = MooseAsks("Do you love C# yet?");
-    if (doesLoveCSharp)
-    {
-        MooseSays("Good job sucking up to your instructor!");
-    }
-    else
-    {
-        MooseSays("You will...oh, yes, you will...");
-    }
-}
-
-void SecretQuestion()
-{
-    bool wantsSecret = MooseAsks("Do you want to know a secret?");
-    if (wantsSecret)
-    {
-        MooseSays("ME TOO!!!! I love secrets...tell me one!");
-    }
-    else
-    {
-        MooseSays("Oh, no...secrets are the best, I love to share them!");
-    }
-}
 
 
 
